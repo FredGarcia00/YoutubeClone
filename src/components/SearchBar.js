b101a36b6a9fb5ff563bc1049745eaf5b05dc886
Fred Garcia <Fred@Garcia.com>
2020-06-10 22:34:07 -0700

    changed styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -20,11 +20,14 @@ class SearchBar extends React.Component {
         <div className="search-bar ui segment">
             <form className="ui form"
             onSubmit={this.onFormSubmit}>
-                <div className="field">
-                <label>Video Search</label>
+                <div className="field" style={{paddingTop:'40px'}}>
+                <label>
+                    <span style={{fontSize:'50px', backgroundColor:'red'}}>FRED</span> 
+                    <span style={{fontSize:'50px'}}>TUBE</span></label>
                 <input type="text" 
                 value={this.state.term} 
-                onChange={this.onInputChange}/>
+                onChange={this.onInputChange}
+                style={{border:'2px solid black'}}/>
                 <button onClick={this.onFormSubmit}>Search</button>
                 </div>
             </form>
